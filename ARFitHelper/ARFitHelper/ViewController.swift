2154d57e2b8d0d27bb35b14a1a46eefec4bc9183
Антон Петренко <trash163@gmail.com>
2020-01-16 21:28:25 +0200

    created project

---
+++ ARFitHelper/ARFitHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARFitHelper
+//
+//  Created by Антон Петренко on 16.01.2020.
+//  Copyright © 2020 Антон Петренко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
